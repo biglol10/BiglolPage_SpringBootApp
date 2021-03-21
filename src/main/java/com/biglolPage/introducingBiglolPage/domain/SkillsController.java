ad043bdba9e0526ca3e03465ccb1525197445207
biiglol <46738034+biiglol@users.noreply.github.com>
2021-03-21 12:57:35 +0900

    skilladd controller added

--- src/main/java/com/biglolPage/introducingBiglolPage/domain/SkillsController.java
+++ src/main/java/com/biglolPage/introducingBiglolPage/domain/SkillsController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +24,11 @@ public class SkillsController {
 	public Iterable<Skills> retrieveAll(){
 		Iterable<Skills> skillList = repository.findAll();
 		return skillList;
+	}
+	
+	@PostMapping("/skills")
+	public void postSkill(@RequestBody Skills skill) {
+		repository.save(skill);
+		
 	}
 }
